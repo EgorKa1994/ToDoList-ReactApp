27aee3c70eebc9dc51e81e283ce8394862ecf4e4
Egor_Kozh <egorkozhemba@gmail.com>
2020-09-25 23:25:55 +0300

    Create firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { initializeApp } from 'firebase';
+
+const app = initializeApp({
+  apiKey: 'AIzaSyBvux9rP1vo2kNbmKF4X-1LFkT1WA8UDdI',
+  authDomain: 'todolistapp-12.firebaseapp.com',
+  databaseURL: 'https://todolistapp-12.firebaseio.com',
+  projectId: 'todolistapp-12',
+  storageBucket: 'todolistapp-12.appspot.com',
+  messagingSenderId: '912952666292',
+  appId: '1:912952666292:web:2bcf74a5c7aa3219cd0fe1',
+  measurementId: 'G-4SSSMWVS2Y',
+});
+
+const firestore = app.firestore();
+
+window.firestore = firestore;
+
+function toObject(doc) {
+  return {
+    id: doc.id,
+    ...doc.data(),
+  };
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +31,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
